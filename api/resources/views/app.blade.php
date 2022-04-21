55283eb957313e6a56135d240ea5bb560cad14c6
Tiago Abreu <devtiagoabreu@gmail.com>
2022-04-21 08:55:59 -0300

    Planejando os endpoints

---
+++ api/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Beer and Code</title>
+  </head>
+  <body>
+    <div>
+        @yield('content') 
+    </div>
+  </body>
+</html>
