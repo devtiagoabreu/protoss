916efe5706ff4a9dfc1c43820faddf19270d0a09
Tiago Abreu <devtiagoabreu@gmail.com>
2022-04-22 11:33:08 -0300

    CRIANDO AS ROTAS

--- api/routes/api.php
+++ api/routes/api.php
@@ -7,6 +7,31 @@ Route::get('/ping', function(){
     return ['pong'=>true];
 });
 
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
+Route::put('/user', 'UserController@create');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@read');
+Route::get('/user/feed', 'FeedController@userFeed');
+Route::get('/user/{id}/feed', 'FeedController@userFeed');
+
+Route::get('/user', 'UserController@read');
+Route::get('/user/{id}', 'UserController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/{id}like', 'PostController@like');
+Route::post('/post/{id}/comment', 'PostController@comment');
+
+Route::get('/search', 'SearchController@search');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
